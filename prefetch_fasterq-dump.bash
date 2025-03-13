c2310095aef602ebebe98dd38676edc31d752605
Rob Bierman <rbierman@princeton.edu>
2025-03-13 11:35:58 -0400

    gha: Add -x to fasterq-dump to get more info

--- prefetch_fasterq-dump.bash
+++ prefetch_fasterq-dump.bash
@@ -2,5 +2,5 @@
 prefetch ERR1295999
 echo "Done with prefetch"
 df -h
-fasterq-dump ERR1295999 --threads 6 --disk-limit 25G
+fasterq-dump ERR1295999 -x --threads 6 --disk-limit 10G
 echo "Done with fasterq-dump"
