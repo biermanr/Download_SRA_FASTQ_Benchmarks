53ef3be924a549f1b76dba963fd6c0f507ab18bb
Rob Bierman <rbierman@princeton.edu>
2025-03-14 12:51:18 -0400

    gha: Use $RUNNER_OS to simplify sratools setup

--- scripts/setup_sratools.bash
+++ scripts/setup_sratools.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [[ $RUNNER_OS == *"ubuntu"* ]]; then
+    SOFTWARE_URL="https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.1.0/sratoolkit.3.1.0-ubuntu64.tar.gz"
+fi
+
+if [[ $RUNNER_OS == *"macos"* ]]; then
+    SOFTWARE_URL="https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.1.0/sratoolkit.3.1.0-mac-arm64.tar.gz"
+fi
+
 wget $SOFTWARE_URL -O sra_toolkit.tar.gz
 mkdir sra_toolkit
 tar -xzf sra_toolkit.tar.gz -C sra_toolkit --strip-components 1
