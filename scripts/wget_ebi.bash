22f743abb2316ead692e8c9fc3d3141799880be5
Rob Bierman <rbierman@princeton.edu>
2025-03-15 14:46:43 -0400

    gha: Change wget to aria2c -s8

--- scripts/wget_ebi.bash
+++ scripts/wget_ebi.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_1.fastq.gz
-wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_2.fastq.gz
+aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_1.fastq.gz
+aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_2.fastq.gz
