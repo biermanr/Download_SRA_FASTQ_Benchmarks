103e59d1cce6a69fc389c19660d792796bd2bdd2
Rob Bierman <rbierman@princeton.edu>
2025-03-15 15:33:18 -0400

    gha: Make aria2c download use ACCESSION
    - Noticed shasum not matching between aria2c
      and the sratools-based approaches
    
    - Added more checks for line counts and size

--- scripts/aria2c_ebi.bash
+++ scripts/aria2c_ebi.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
-aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_1.fastq.gz
-aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR325/008/SRR32596108/SRR32596108_2.fastq.gz
+PREFIX="${ACCESSION:0:6}"
+SUFFIX="00${ACCESSION:10:1}"
 
-gzip -d SRR32596108_1.fastq.gz
-gzip -d SRR32596108_2.fastq.gz
+aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/$PREFIX/$SUFFIX/$ACCESSION/$ACCESSION.fastq.gz
+aria2c -s8 -x8 https://ftp.sra.ebi.ac.uk/vol1/fastq/$PREFIX/$SUFFIX/$ACCESSION/$ACCESSION.fastq.gz
+
+gzip -d $ACCESSION_1.fastq.gz
+gzip -d $ACCESSION_2.fastq.gz
